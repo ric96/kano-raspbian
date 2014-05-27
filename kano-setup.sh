4e20b809720a60fdcf20123e3de66771b17c776b
Sahaj Sarup <sahajsarup@gmail.com>
2014-05-28 03:40:29 +0530

    Update kano-setup.sh
    script now must be run as root

--- kano-setup.sh
+++ kano-setup.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/bash -e
 
-sudo echo "deb http://dev.kano.me/archive/ release main" > /etc/apt/sources.list.d/kano.list
+if [ $EUID -ne 0 ]; then
+  echo "This script must be run as root !!!"
+  exit 1
+fi
+
+echo "deb http://dev.kano.me/archive/ release main" > /etc/apt/sources.list.d/kano.list
 wget http://dev.kano.me/archive/repo.gpg.key
-sudo /user/bin/apt-key add repo.gpg.key
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install gnome-panel-control kano-blocks kano-credits kano-desktop kano-extras kano-test kano-themes kano-unlocker kano-vnc kdesk kano-feedback kano-fonts kano-init kano-motd kano-profile kano-settings kano-toolset kano-updater kano-youtube make-minecraft make-music make-pong make-snake openbox-dev openbox xterm -y
+/user/bin/apt-key add repo.gpg.key
+apt-get update
+apt-get dist-upgrade -y
+apt-get install gnome-panel-control kano-blocks kano-credits kano-desktop kano-extras kano-test kano-themes kano-unlocker kano-vnc kdesk kano-feedback kano-fonts kano-init kano-motd kano-profile kano-settings kano-toolset kano-updater kano-youtube make-minecraft make-music make-pong make-snake openbox-dev openbox xterm -y
 wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-sudo pip install python-slugify
-sudo pip install requests
-sudo pip install pam
+python get-pip.py
+pip install python-slugify
+pip install requests
+pip install pam
+rm repo.gpg.key
+rm get-pip.py
 echo "Installation Copleted"
